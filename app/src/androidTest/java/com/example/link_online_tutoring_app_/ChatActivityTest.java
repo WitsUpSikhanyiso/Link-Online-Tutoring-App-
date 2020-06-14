db4e334cf6508b2b5aad85ab65cf87c627c8fa11
mabhutidlodlo <1428523@students.wits.ac.za>
2020-06-15 00:47:58 +0200

    Update ChatActivityTest.java

--- app/src/androidTest/java/com/example/link_online_tutoring_app_/ChatActivityTest.java
+++ app/src/androidTest/java/com/example/link_online_tutoring_app_/ChatActivityTest.java
@@ -1,26 +1,45 @@
 package com.example.link_online_tutoring_app_;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.content.Context.MODE_PRIVATE;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 @RunWith(AndroidJUnit4.class)
 public class ChatActivityTest {
-    /*
+    
     @Rule
     public ActivityTestRule rule = new ActivityTestRule(ChatActivity.class, true, false);
     SharedPreferences.Editor PE;
@@ -95,5 +114,5 @@ public class ChatActivityTest {
 
 
 
-*/
+
 }
